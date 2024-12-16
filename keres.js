8dbe4cb8e2c0a61e83295d65c928be3010b1647b
SDaniel2000 <104784030+SDaniel2000@users.noreply.github.com>
2024-12-16 15:43:09 +0100

    a

--- keres.js
+++ keres.js
@@ -1,32 +1,29 @@
 fetch('output.json')
   .then(response => response.json())
   .then(data => {
-    let idx = lunr(function() {
-      this.field('title');
-      this.field('content');
-      this.field('url');
-      this.ref('url');
-      data.forEach(doc => this.add(doc));
-    });
+    // JsSearch indexálás
+    let search = new JsSearch.Search('url');
+    search.addIndex('title');
+    search.addIndex('content');
+    search.addDocuments(data);
 
+    // Keresési esemény
     document.getElementById("searchBox").addEventListener("input", function() {
       let query = this.value;
-      let results = idx.search(query);
+      let results = search.search(query);
       displayResults(results);
     });
 
+    // Eredmények megjelenítése
     function displayResults(results) {
       let resultsDiv = document.getElementById("results");
       resultsDiv.innerHTML = "";
       results.forEach(result => {
         let item = document.createElement("div");
-        let baseURL = 'https://example.com/';  // Az alap URL, ahonnan a fájlok elérhetők
-        let fullURL = baseURL + result.ref;    // Teljes URL létrehozása
-        item.innerHTML = `Title: <a href="${fullURL}" target="_blank">${result.ref}</a> - Snippet: ${getSnippet(result.ref)}`;
+        item.innerHTML = `Title: <a href="${result.url}" target="_blank">${result.title}</a> - Snippet: ${getSnippet(result.url)}`;
         resultsDiv.appendChild(item);
       });
     }
-    
 
     function getSnippet(url) {
       let doc = data.find(d => d.url === url);
