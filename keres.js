e164e483f99a80a5da8cab2adc2669b7386cd209
SDaniel2000 <104784030+SDaniel2000@users.noreply.github.com>
2024-12-17 17:44:16 +0100

    Update keres.js

--- keres.js
+++ keres.js
@@ -1,27 +1,27 @@
 // Feltételezzük, hogy a JSON fájl neve output.json
-let searchIndex = [];
+let searchIndex = new JsSearch.Search('id'); // Az 'id' a dokumentum egyedi azonosítója
 
 // Az JSON fájl betöltése
 fetch('output.json')
   .then(response => response.json())
   .then(data => {
-    searchIndex = data;
+    searchIndex.addIndex('title');
+    searchIndex.addIndex('content');
+    searchIndex.addDocuments(data);
   })
   .catch(error => console.error("Hiba történt a JSON fájl betöltésekor:", error));
 
 // Keresés a megadott kifejezés alapján
 function searchContent() {
-    const query = document.getElementById("searchQuery").value.toLowerCase();
-    const results = searchIndex.filter(page => {
-        return page.content.toLowerCase().includes(query) || page.title.toLowerCase().includes(query);
-    });
+    const query = document.getElementById("searchQuery").value;
+    const results = searchIndex.search(query);
 
     displayResults(results);
 }
 
 // Eredmények megjelenítése a keresés alapján
 function displayResults(results) {
-    const resultContainer = document.getElementById("searchResults");
+    const resultContainer = document.getElementById("results");
     resultContainer.innerHTML = ''; // Eredmények törlése a régi keresés előtt
 
     if (results.length === 0) {
@@ -41,7 +41,6 @@ function displayResults(results) {
         // Cím kiemelése a keresett szóra
         const regex = new RegExp(query, 'gi');
         const highlightedTitle = result.title.replace(regex, match => `<span class="highlight">${match}</span>`);
-        const highlightedContent = result.content.replace(regex, match => `<span class="highlight">${match}</span>`);
 
         resultElement.innerHTML = `<a href="${result.url}" target="_blank">${highlightedTitle}</a>`;
         resultContainer.appendChild(resultElement);
